1b48df0f86643564a20c2e9fdb6377e029241d6d
Imanol <imanol.luis@ikasle.egibide.org>
2018-04-23 10:52:33 +0200

    .

--- Programa/E_Sport/src/UML/Equipo.java
+++ Programa/E_Sport/src/UML/Equipo.java
@@ -1,22 +1,5 @@
-<<<<<<< Updated upstream
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package UML;
-
-/**
- *
- * @author v6222
- */
-public class Equipo {
-    
-}
-=======
 package UML;
 
 public class Equipo {
     
 }
->>>>>>> Stashed changes
