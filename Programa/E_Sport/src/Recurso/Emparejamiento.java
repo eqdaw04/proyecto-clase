6790145ba03de645e9cd31f77284cffc7cbb7985
v6222 <mygoldxp@msn.com>
2018-04-24 13:38:59 +0200

    EMPAREJAMIENTO

--- Programa/E_Sport/src/Recurso/Emparejamiento.java
+++ Programa/E_Sport/src/Recurso/Emparejamiento.java
@@ -32,20 +32,53 @@ public class Emparejamiento {
         this.lEquipo = listaEquipo;
     }
     
-    public void calcularJornada(){
+    public void calcularPartido(){
         int e = lEquipo.size();
         // obtener numero de jornadas, partidos y crear Array
         int j = (e-1)*2;
         lJornada = new Jornada[j];
-        int p = e/2;
+        int p = e-1;
         lPartido = new Partido[p];
         // establecemos que 1 jornada = 1 semana = 7 días
         //desordenar numero equipo
         Collections.shuffle(lEquipo);
-        //random de un numero para seleccionar una posicion aleatoria del array
+        //random del rango del tamaño del array para seleccionar una posicion aleatoria del array para el primer array
         Random rand = new Random();
-        int n = rand.nextInt(e-1) + 1;
-  
+        int a1 = rand.nextInt(e-1);
+        //random igual al anterior excluyendo el número a1
+        int a2;
+        do{
+            a2 = rand.nextInt(e-1);
+        }
+        while(a2==a1);
+        String dato = "Equipos:\n";
+        int n = 0;
+        for(int x = a1+1; x!=a1; x++){
+            if(x==lEquipo.size()){
+                x = 0;
+            }
+            
+            if((a1+n) == lEquipo.size()){
+                n = a1/-1;
+            }
+            dato += asignarEquipo(a1+n,a2) + "\n";
+            n++;
+        }
+        JOptionPane.showMessageDialog(null, dato);
+    }
+    
+    public String asignarEquipo(int a1, int a2){
+        String dato = "";
+        dato += lEquipo.get(a1).getIdEquipo() + "vs" + lEquipo.get(a2).getIdEquipo() + "   ";
+        for(int x = a2+1; x!=a2 ; x++){
+            if(x==lEquipo.size()){
+                x=0;
+            }
+            if(lEquipo.get(a1) != lEquipo.get(x)){
+                dato += lEquipo.get(a1).getIdEquipo() + "vs" + lEquipo.get(x).getIdEquipo() + "   ";
+            }
+        }
+        return dato;
     }
 
 }
