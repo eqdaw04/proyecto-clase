c9c0ee4cc4542584f6a9d41181e5b5712bdc9d37
Imanol <imanol.luis@ikasle.egibide.org>
2018-05-09 09:10:11 +0200

    Auto stash before merge of "master" and "origin/master"

--- Programa/E_Sport/src/Excepciones/Excepcion.java
+++ Programa/E_Sport/src/Excepciones/Excepcion.java
@@ -5,160 +5,152 @@
  */
 package Excepciones;
 
-import Controladora.Main;
-
 /**
  *
  * @author v6222
  */
 public class Excepcion extends Exception {
     
-    int cod;
-    String mensaje;
+    int codigo;
 
-    public Excepcion() {
-    }
-
-    public Excepcion(int cod) {
-        this.cod = cod;
-    }
-    
-    public Excepcion(String mensaje) {
-        this.mensaje = mensaje;
+    public Excepcion(int codigo) {
+        this.codigo = codigo;
     }
     
     @Override
     public String getMessage(){
-        String dato = "(" + cod + ") ";
-        switch(cod){
+        String dato = "(" + codigo + ") ";
+        switch(codigo)
+        {
             case 1:
-                dato = "El usuario no puede estar vacío.";
+                dato = dato + "El usuario no puede estar vacío.";
                 break;
                 
             case 2:
-                dato = "La contraseña no puede estar vacío.";
+                dato = dato + "La contraseña no puede estar vacía.";
                 break;
                 
             case 3:
-                dato = "El DNI debe tener el siguiente formato: LNNNNNNNL o NNNNNNNNL\nEjemplo X1234567Z o 12345678Z";
+                dato = dato + "El DNI debe tener el siguiente formato: LNNNNNNNL o NNNNNNNNL\nEjemplo X1234567Z o 12345678Z";
                 break;
                 
             case 4:
-                dato = "El nombre ha de empezar con una mayúscula seguido de minúsculas con un rango mínimo total de 3 letras y sin números ni signos.";
+                dato = dato + "El nombre ha de empezar con una mayúscula seguido de minúsculas con un rango mínimo total de 3 letras y sin números ni signos.";
                 break;
                 
             case 5:
-                dato = "El apellido ha de empezar con una mayúscula seguido de minúsculas con un rango mínimo total de 3 letras y sin números ni signos.";
+                dato = dato + "El apellido ha de empezar con una mayúscula seguido de minúsculas con un rango mínimo total de 3 letras y sin números ni signos.";
                 break;
                 
             case 6:
-                dato = "El email debe ser coherente, ejemplo: ejemplo@compania.es";
+                dato = dato + "El email debe ser coherente, ejemplo: ejemplo@compania.es";
                 break;
                 
             case 7:
-                dato = "El usuario debe tener un mínimo de 3 letras.";
+                dato = dato + "El usuario debe tener un mínimo de 3 letras.";
                 break;
                 
             case 8:
-                dato = "La contraseña ha de tener un mínimo de 3 carácteres, tipo números, letras en mayúsculas o minúsculas. ";
+                dato = dato + "La contraseña ha de tener un mínimo de 3 carácteres, tipo números, letras en mayúsculas o minúsculas. ";
                 break;
                 
             case 9:
-                dato = "Debe seleccionar un perfil.";
+                dato = dato + "Debe seleccionar un perfil.";
                 break;
                 
             case 10:
-                dato = "No has introducido el sueldo del jugador.";
+                dato = dato + "No has introducido el sueldo del jugador.";
                 break;
                 
             case 11:
-                dato = "Seleccione si es un Alta o una Baja.";
+                dato = dato + "Seleccione si es un Alta o una Baja.";
                 break;
                 
             case 12:
-                dato = "Los datos de login son incorrectos.";
+                dato = dato + "Los datos de login son incorrectos.";
                 break;
                 
             case 13:
-                dato = "Has superado el límite de intentos.";
+                dato = dato + "Has superado el límite de intentos.";
                 break;
                 
             case 14:
-                dato = "Usuario no registrado en la Base de datos.";
+                dato = dato + "Usuario no registrado en la Base de datos.";
                 break;
                 
             case 15:
-                dato = "El Usuario ya existe en la Base de datos.";
+                dato = dato + "El Usuario ya existe en la Base de datos.";
                 break;
                 
             case 16:
-                dato = "Debe seleccionar un equipo.";
+                dato = dato + "Debe seleccionar un equipo.";
                 break;
                 
             case 17:
-                dato = "No existe ningún jugador con ese DNI.";
+                dato = dato + "No existe ningún jugador con ese DNI.";
                 break;
                 
             case 18:
-                dato = "No existe ningún usuario con ese nombre.";
+                dato = dato + "No existe ningún usuario con ese nombre.";
                 break;
                 
             case 19:
-                dato = "Ya existe un equipo con ese nombre.";
+                dato = dato + "Ya existe un equipo con ese nombre.";
                 break;
                 
             case 20:
-                dato = "No existe ningún equipo con ese nombre.";
+                dato = dato + "No existe ningún equipo con ese nombre.";
                 break;
                 
             case 21:
-                dato = "Has excedido el límite salarial.";
+                dato = dato + "Has excedido el límite salarial.";
                 break;
                 
             case 22:
-                dato = "Ya tienes 6 jugadores.";
+                dato = dato + "Ya tienes 6 jugadores.";
                 break;
                 
             case 23:
-                dato = "Ya existe un jugador con ese DNI.";
+                dato = dato + "Ya existe un jugador con ese DNI.";
                 break;
                 
             case 24:
-                dato = "Ese jugador no pertenece a tu equipo.";
+                dato = dato + "Ese jugador no pertenece a tu equipo.";
                 break;
                 
             case 25:
-                dato = "No se ha realizado cambios en la BBDD.";
+                dato = dato + "No se ha realizado cambios en la BBDD.";
                 break;
                 
             case 26:
-                dato = "El nicname no puede estar vacío.";
+                dato = dato + "El nicname no puede estar vacío.";
                 break;
                 
             case 27:
-                dato = "No hay jugadores en la base de datos.";
+                dato = dato + "No hay jugadores en la base de datos.";
                 break;
                 
             case 28:
-                dato = "No hay equipos en la base de datos";
+                dato = dato + "No hay equipos en la base de datos";
                 break;
                 
             case 30:
-                dato = "El salario del equipo es superior a 200.000 €. \n Escoge un jugador con un salario menor o retira uno de los jugadores";
+                dato = dato + "El salario del equipo es superior a 200.000 €. \n Escoge un jugador con un salario menor o retira uno de los jugadores";
                 break;
+                
             case 31:
-                dato = "Has alcanzado el límite de 6 jugadores. Por favor retira un jugador antes de añadir uno nuevo";
+                dato = dato + "Has alcanzado el límite de 6 jugadores. Por favor retira un jugador antes de añadir uno nuevo";
                 break;
+                
             case 50:
-                dato = "El nombre del equipo es obligatorio.";
+                dato = dato + "El nombre del equipo es obligatorio.";
                 break;
         }
         return dato;
     }
 
-    public int getCod() {
-        return cod;
+    public int getCodigo() {
+        return codigo;
     }
-    
     
 }
