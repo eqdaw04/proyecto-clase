818acca29b349dc683ff790de62c4bfb86c31cf9
Imanol <imanol.luis@ikasle.egibide.org>
2018-05-15 13:35:19 +0200

    Auto stash before merge of "master" and "origin/master"

--- Programa/E_Sport/src/BD/BDJugador.java
+++ Programa/E_Sport/src/BD/BDJugador.java
@@ -10,7 +10,6 @@ import UML.Jugador;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -22,37 +21,123 @@ import java.util.Calendar;
  */
 
 public class BDJugador {
-
+    
     public BDJugador() {
+    } 
+    
+    /**
+     * Metodo para insertar un jugador.
+     * @param j Jugador
+     * @throws Exception 
+     */    
+    
+    public void insertarJugador(Jugador j) throws Exception {
+        
+        BDConexion con = new BDConexion();
+        
+        PreparedStatement sentencia;
+        sentencia = con.getConnection().prepareStatement("INSERT INTO Jugador (DNI, NOMBRE, APELLIDO1, APELLIDO2, NICKNAME, SUELDO, FECHA_ALTA, COMENTARIO) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+        
+        sentencia.setString(1, j.getDni());
+        sentencia.setString(2, j.getNombre());
+        sentencia.setString(3, j.getApellido1());
+        sentencia.setString(4, j.getApellido2());
+        sentencia.setString(5, j.getNickname());
+        sentencia.setFloat(6, j.getSueldo());
+        sentencia.setDate(7, formatearFecha(j.getFechaAlta()));
+        sentencia.setString(8, j.getComentario());
+        
+        // Si no inserta fila, lanza excepción
+        if(sentencia.executeUpdate()!=1)
+        {
+            throw new Excepcion(25);
+        }
+        
+        sentencia.close();
+        con.desconectar();
+    }
+    
+    /**
+     * Metodo para eliminar un jugador.
+     * @param j Jugador
+     * @throws Exception 
+     */
+    
+    public void eliminarJugador(Jugador j) throws Exception {
+        BDConexion con = new BDConexion();
+        
+        PreparedStatement sentencia;
+        sentencia = con.getConnection().prepareStatement("DELETE FROM Jugador WHERE Id_jugador = ?");
+        
+        sentencia.setInt(1, j.getIdJugador());                
+        
+        // Si no elimina fila, lanza excepción
+        if(sentencia.executeUpdate()!=1)
+        {
+            throw new Excepcion(25);
+        }
+        
+        sentencia.close();
+        con.desconectar();
     }
     
      /**
-     * Metodo para buscar un jugador por dni.
-     * @param dni String
-     * @return devuelve una lista de jugadores 
+     * Metodo para modificar los datos de un jugador.
+     * @param j Jugador
      * @throws Exception 
      */   
     
-    public Jugador BuscarJugador(String dni) throws Exception {
-        // abrir la conexión
+    public void modificarJugador(Jugador j) throws Exception {
         BDConexion con = new BDConexion();
-        // instanciar un arraylist
+        
+        PreparedStatement sentencia;
+        sentencia = con.getConnection().prepareStatement("UPDATE Jugador SET DNI=?, NOMBRE=?, APELLIDO1=?, APELLIDO2=?, NICKNAME=?, SUELDO=?, COMENTARIO=? WHERE ID_JUGADOR=?");
+        
+        sentencia.setString(1, j.getDni());
+        sentencia.setString(2, j.getNombre());
+        sentencia.setString(3, j.getApellido1());
+        sentencia.setString(4, j.getApellido2());
+        sentencia.setString(5, j.getNickname());
+        sentencia.setFloat(6, j.getSueldo());
+        sentencia.setString(7, j.getComentario());
+        sentencia.setInt(8, j.getIdJugador());
+        
+        // Si no modifica fila, lanza excepción
+        if(sentencia.executeUpdate()!=1)
+        {
+            throw new Excepcion(25);
+        }
+        
+        sentencia.close();
+        con.desconectar();
+    }
+    
+    /**
+     * Metodo para buscar un jugador por DNI.
+     * @param dni String
+     * @return Devuelve un jugador
+     * @throws Exception
+     */
+    
+    public Jugador BuscarJugador(String dni) throws Exception {
+        BDConexion con = new BDConexion();
         ArrayList<Jugador> a = new ArrayList();
-        // preparar la sentencia
+        
         PreparedStatement sentencia;
         sentencia = con.getConnection().prepareStatement("SELECT * FROM Jugador WHERE Dni = ?");
-        // cargar datos al ?
+        
         sentencia.setString(1,dni);
-        // instanciar rs, ejecutarlo y cargar los datos al rs
+        
+        // Ejecuta la consulta e inserta filas en un ArrayList de jugadores
         ResultSet rs;
         rs = sentencia.executeQuery();
-        // mandar rs a una funcion
-        a = recorrer(rs,a);
-        // cerrar lo abierto
+        a = recorrer(rs, a);
+        
         rs.close();
         sentencia.close();
         con.desconectar();
-        // comprobar si la lista tiene datos y devolver objeto
+        
+        // Si está vacío, devuelve valor nulo, Sino devuelve el primero del ArrayList
         if(a.isEmpty())
         {
             return null;
@@ -60,77 +145,73 @@ public class BDJugador {
         else
         {
             return a.get(0);
-        }        
+        }
     }
     
     /**
      * Metodo para buscar una lista de todos los jugadores.
-     * @return devuelve una lista de jugadores 
+     * @return Devuelve un ArrayList de jugadores
      * @throws Exception 
      */    
     
     public ArrayList<Jugador> BuscarJugador() throws Exception {
-        // abrir conexion
         BDConexion con = new BDConexion();
-        // instanciar una lista
         ArrayList<Jugador> a = new ArrayList();
-        // preparar la sentencia
+        
         PreparedStatement sentencia;
         sentencia = con.getConnection().prepareStatement("SELECT * FROM Jugador");
-        // instanciar rs, ejecutar la sentencia y cargarlo al rs
+        
+        // Ejecuta la consulta e inserta las filas en un ArrayList de jugadores
         ResultSet rs;
         rs = sentencia.executeQuery();
-        // mandar el rs a una función
-        a = recorrer(rs,a);
-        // cerrar lo abierto
+        a = recorrer(rs, a);
+        
         rs.close();
         sentencia.close();
         con.desconectar();
-        // devolver lista
+        
         return a;
     }
     
-        /**
-     * Metodo para buscar la lista de jugadores de un equipo. 
+    /**
+     * Metodo para buscar todos los jugadores de un equipo. 
      * @param id String
-     * @return devuelve una lista de jugadores
+     * @return Devuelve un ArrayList de jugadores
      * @throws Exception 
      */
     
     public ArrayList<Jugador> BuscarEqui(String id) throws Exception {
-        // abrir conexión
        BDConexion con = new BDConexion();
-       // instanciar una lista
+       
        ArrayList<Jugador> a= new ArrayList();
-       // preparar la sentencia
-        PreparedStatement sentencia;
-        sentencia = con.getConnection().prepareStatement("SELECT * FROM Jugador WHERE Id_equipo = ?");
-        // cargar dato al ?
-        sentencia.setString(1,id);
-        // instanciar rs, ejecutar la sentencia y cargar los datos obtenidos al rs
-        ResultSet rs;
-        rs = sentencia.executeQuery();
-        // enviar el rs a una función
-        a= recorrer (rs,a);
-        // cerrar lo abierto
-        rs.close();
-        con.desconectar();
-        // devolver la lista
-        return a;
+       
+       PreparedStatement sentencia;
+       sentencia = con.getConnection().prepareStatement("SELECT * FROM Jugador WHERE Id_equipo = ?");
+       
+       sentencia.setString(1,id);
+       
+       // Ejecuta la consulta e inserta las filas en un ArrayList de jugadores
+       ResultSet rs;
+       rs = sentencia.executeQuery();
+       a = recorrer(rs, a);
+       
+       rs.close();
+       con.desconectar();
+       
+       return a;
     }
     
-        /**
-     * Metodo para recorrer los datos de un jugador.
+    /**
+     * Metodo para recorrer el resultado de una consulta para devolver un ArrayList de jugadores
      * @param rs ResultSet
-     * @param a ArrayList de jugador
-     * @return devuelve los datos del jugador introducido en la lista
-     * @throws SQLException 
+     * @param a ArrayList de jugadores
+     * @return devuelve un ArrayList de jugadores
+     * @throws Exception 
      */
     
-    public ArrayList<Jugador> recorrer(ResultSet rs, ArrayList <Jugador> a) throws SQLException {
-        // comprobar si existe datos en el rs
-        while (rs.next()){
-            // instanciar objeto jugador y llenarlo de datos del rs
+    public ArrayList<Jugador> recorrer(ResultSet rs, ArrayList <Jugador> a) throws Exception {
+        while (rs.next())
+        {
             Jugador j= new Jugador();
             j.setIdJugador(Integer.parseInt(rs.getString("id_jugador")));
             j.setDni(rs.getString("dni"));
@@ -139,44 +220,43 @@ public class BDJugador {
             j.setApellido2(rs.getString("apellido2"));
             j.setNickname(rs.getString("nickname"));
             j.setSueldo(Float.parseFloat(rs.getString("sueldo")));
+            // Formato de la fecha
             long as = rs.getTimestamp("fecha_alta").getTime();
             Calendar c = Calendar.getInstance();
             c.setTimeInMillis(as);
             j.setFechaAlta(c.getTime());
-            j.setComentario(rs.getString("comentario"));      
-            // añadir el objeto a la lista
+            j.setComentario(rs.getString("comentario"));
+            
             a.add(j);
         }
-        // devolver lista
+        
         return a;
     }
     
-        /**
+    /**
      * Metodo para buscar jugadores que no estén en ningún equipo.
-     * @return devuelve una lista de jugadores
+     * @return Devuelve un ArrayList de jugadores
      * @throws Exception 
      */
 
     public ArrayList<Jugador> BuscarJugadoresDisponibles() throws Exception {
-        // abrir conexion
-        BDConexion con = new BDConexion();
-        // instanciar una lista
-        ArrayList<Jugador> a= new ArrayList();
-        // preparar la sentencia
-        PreparedStatement sentencia;
-        sentencia = con.getConnection().prepareStatement("SELECT * FROM Jugador WHERE Id_equipo is null");
-        sentencia.executeUpdate();
-        // instanciar rs, ejecutar sentencia y cargarlo al rs
-        ResultSet rs;
-        rs = sentencia.executeQuery();
-        // mandar el rs a una funcion
-        a= recorrer (rs,a);
-        // cerrar lo abierto
-        rs.close();
-        sentencia.close();
-        con.desconectar();
-        // devolver la lista
-        return a;
+       BDConexion con = new BDConexion();
+       ArrayList<Jugador> a= new ArrayList();
+        
+       PreparedStatement sentencia;
+       sentencia = con.getConnection().prepareStatement("SELECT * FROM Jugador WHERE Id_equipo is null");
+       sentencia.executeUpdate();
+        
+       // Ejecuta la consulta e inserta las filas en un ArrayList de jugadores
+       ResultSet rs;
+       rs = sentencia.executeQuery();
+       a = recorrer(rs, a);
+       
+       rs.close();
+       sentencia.close();
+       con.desconectar();
+       
+       return a;
     }
     
     /**
@@ -186,172 +266,99 @@ public class BDJugador {
      * @throws Exception 
      */
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    // pendiente bdpartido, bdperfil y bdpersona
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    public Jugador buscarJugadorNickname(String nickname) throws Exception{ 
+    public Jugador buscarJugadorNickname(String nickname) throws Exception {
         BDConexion con = new BDConexion(); 
-        ArrayList<Jugador> a = new ArrayList(); 
+        ArrayList<Jugador> a = new ArrayList();
+        
         PreparedStatement sentencia;
-        sentencia = con.getConnection().prepareStatement("SELECT * FROM Jugador WHERE Nickname = ?"); 
-        sentencia.setString(1,nickname); 
+        sentencia = con.getConnection().prepareStatement("SELECT * FROM Jugador WHERE Nickname = ?");
+        sentencia.setString(1,nickname);
+        
+        // Ejecuta la consulta e inserta las filas en un ArrayList de jugadores
         ResultSet rs;
-        rs = sentencia.executeQuery(); 
-        a = recorrer(rs,a); 
+        rs = sentencia.executeQuery();
+        a = recorrer(rs, a);
+        
         rs.close();
-        sentencia.close(); 
-        con.desconectar(); 
-        return a.get(0); 
-    } 
-    
-    /**
-     * Metodo para insertar jugadores.
-     * @param j Jugador
-     * @throws Exception 
-     */    
-    
-    public void insertarJugador(Jugador j) throws Exception {
-        BDConexion con = new BDConexion();
-        PreparedStatement sentencia;
-        sentencia = con.getConnection().prepareStatement("INSERT INTO Jugador (DNI, NOMBRE, APELLIDO1, APELLIDO2, NICKNAME, SUELDO, FECHA_ALTA, COMENTARIO) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-        sentencia.setString(1, j.getDni());
-        sentencia.setString(2, j.getNombre());
-        sentencia.setString(3, j.getApellido1());
-        sentencia.setString(4, j.getApellido2());
-        sentencia.setString(5, j.getNickname());
-        sentencia.setFloat(6, j.getSueldo());
-        sentencia.setDate(7, formatearFecha(j.getFechaAlta()));
-        sentencia.setString(8, j.getComentario());
-                                
-        if(sentencia.executeUpdate()!=1)
-        {
-            throw new Excepcion(25);
-        }
         sentencia.close();
         con.desconectar();
-    }
-    
-        /**
-     * Metodo para eliminar jugadores.
-     * @param j Jugador
-     * @throws Exception 
-     */
-    
-    public void eliminarJugador(Jugador j) throws Exception {
-        BDConexion con = new BDConexion();
-        PreparedStatement sentencia;
-        sentencia = con.getConnection().prepareStatement("DELETE FROM Jugador WHERE Id_jugador = ?"); 
-        sentencia.setInt(1, j.getIdJugador());                
-        if(sentencia.executeUpdate()!=1)
+        
+        // Si está vacío, devuelve valor nulo, Sino devuelve el primero del ArrayList
+        if(a.isEmpty())
         {
-            throw new Excepcion(25);
+            return null;
         }
-        sentencia.close();
-        con.desconectar();
+        else
+        {
+            return a.get(0);
+        }
     }
     
-        /**
-     * Metodo para borrar jugadores de un equipo.
+     /**
+     * Metodo para dar da alta un jugador de un equipo.
      * @param nickname String
-     * @return devuelve correcto si el UPDATE se hace bien
-     * @throws java.lang.Exception
-     */
+     * @param id String
+     * @return Devuelve valor booleano si la sentencia se ejecuta correctamente
+     * @throws Exception 
+     */  
 
-    public boolean QuitarJugadorEquipo(String nickname) throws Exception {
+    public boolean PonerJugadorEquipo(String nickname, String id) throws Exception {
+        BDConexion con = new BDConexion();
         boolean correcto;
         correcto = false;
-        BDConexion con = new BDConexion();
+        
         PreparedStatement sentencia;
-        sentencia = con.getConnection().prepareStatement("UPDATE Jugador SET Id_equipo = null WHERE UPPER(Nickname) = UPPER(?)");
-        sentencia.setString(1, nickname);
-        if(sentencia.executeUpdate()==1){
-            correcto =true;
+        sentencia = con.getConnection().prepareStatement("UPDATE Jugador SET Id_equipo = ? WHERE UPPER(Nickname) = UPPER(?)");
+        
+        sentencia.setString(1, id);
+        sentencia.setString(2, nickname);
+        
+        // Si no se ejecuta, se trata después con la excepción correpondiente
+        if(sentencia.executeUpdate()==1)
+        {
+            correcto=true;
         }
+        
         sentencia.close();
         con.desconectar();
         
         return correcto;
     }
     
-     /**
-     * Metodo para insertar un jugador en un equipo.
+    /**
+     * Metodo para dar da baja un jugador de un equipo.
      * @param nickname String
-     * @param id String
-     * @return devuelve correcto si el UPDATE se hace bien
-     * @throws Exception 
-     */  
+     * @return Devuelve valor booleano si la sentencia se ejecuta correctamente
+     * @throws java.lang.Exception
+     */
 
-    public boolean PonerJugadorEquipo(String nickname, String id) throws Exception{
+    public boolean QuitarJugadorEquipo(String nickname) throws Exception {
+        BDConexion con = new BDConexion();
         boolean correcto;
         correcto = false;
-        BDConexion con = new BDConexion();
+        
         PreparedStatement sentencia;
-        sentencia = con.getConnection().prepareStatement("UPDATE Jugador SET Id_equipo = ? WHERE UPPER(Nickname) = UPPER(?)");
-        sentencia.setString(1, id);
-        sentencia.setString(2, nickname);
-        if(sentencia.executeUpdate()==1){
-            correcto =true;
+        sentencia = con.getConnection().prepareStatement("UPDATE Jugador SET Id_equipo = null WHERE UPPER(Nickname) = UPPER(?)");
+        
+        sentencia.setString(1, nickname);
+        
+        // Si no se ejecuta, se trata después con la excepción correpondiente
+        if(sentencia.executeUpdate()==1)
+        {
+            correcto=true;
         }
+        
         sentencia.close();
         con.desconectar();
+        
         return correcto;
     }
     
-     /**
-     * Metodo para modificar datos de un jugador.
-     * @param j Jugador
-     * @throws Exception 
-     */   
-    
-    public void modificarJugador(Jugador j) throws Exception {
-        BDConexion con = new BDConexion();
-        PreparedStatement sentencia;
-        sentencia = con.getConnection().prepareStatement("UPDATE Jugador SET DNI=?, NOMBRE=?, APELLIDO1=?, APELLIDO2=?, NICKNAME=?, SUELDO=?, COMENTARIO=? WHERE ID_JUGADOR=?"); 
-        sentencia.setString(1, j.getDni()); 
-        sentencia.setString(2, j.getNombre()); 
-        sentencia.setString(3, j.getApellido1()); 
-        sentencia.setString(4, j.getApellido2()); 
-        sentencia.setString(5, j.getNickname()); 
-        sentencia.setFloat(6, j.getSueldo()); 
-        sentencia.setString(7, j.getComentario()); 
-        sentencia.setInt(8, j.getIdJugador());                          
-        if(sentencia.executeUpdate()!=1)
-        {
-            throw new Excepcion(25);
-        }
-        sentencia.close();
-        con.desconectar();
-    }
-    
-       /**
-     * Metodo para formatear la fecha de alta.
+    /**
+     * Metodo para dar formato a fechas para trabajar con la Base de Datos.
      * @param fechaE Date
      * @return fecha
-     */ 
+     */
 
     private Date formatearFecha(java.util.Date fechaE){
         SimpleDateFormat formar = new SimpleDateFormat("yyyy-MM-dd");
