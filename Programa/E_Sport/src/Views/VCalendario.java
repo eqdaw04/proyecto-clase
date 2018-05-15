93b57a5902492fa0d4be9cb870cad8644532be77
Mikel <mikelferreiroguridi@gmail.com>
2018-05-15 10:03:47 +0200

    Auto stash before merge of "master" and "origin/master"

--- Programa/E_Sport/src/Views/VCalendario.java
+++ Programa/E_Sport/src/Views/VCalendario.java
@@ -28,17 +28,36 @@ public class VCalendario extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        Njornada = new javax.swing.JLabel();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+        jLabel1.setText("Jornada");
+
+        Njornada.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
+        Njornada.setText("x");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 553, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(216, 216, 216)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(Njornada)
+                .addContainerGap(246, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 381, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(Njornada))
+                .addContainerGap(325, Short.MAX_VALUE))
         );
 
         pack();
@@ -87,5 +106,7 @@ public class VCalendario extends javax.swing.JDialog {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel Njornada;
+    private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
