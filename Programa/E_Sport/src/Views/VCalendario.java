25293193c61e02a8964095de7c4503eb43649e25
Mikel <mikelferreiroguridi@gmail.com>
2018-05-15 13:43:11 +0200

    Auto stash before merge of "master" and "origin/master"

--- Programa/E_Sport/src/Views/VCalendario.java
+++ Programa/E_Sport/src/Views/VCalendario.java
@@ -5,7 +5,11 @@
  */
 package Views;
 
-import javax.swing.JPanel;
+import Controladora.Main;
+import UML.Partido;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,6 +20,8 @@ public class VCalendario extends javax.swing.JDialog {
     /**
      * Creates new form VCalendario
      */
+    private ArrayList<Partido> partidos;
+    
     public VCalendario() {
         initComponents();
         setVisible(true);
@@ -33,11 +39,8 @@ public class VCalendario extends javax.swing.JDialog {
 
         jLabel1 = new javax.swing.JLabel();
         Njornada = new javax.swing.JLabel();
-        pPartido1 = new javax.swing.JPanel();
-        lbE1 = new javax.swing.JLabel();
-        lbE2 = new javax.swing.JLabel();
-        VS = new javax.swing.JLabel();
-        lbFecha = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        taPartidos = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -47,46 +50,9 @@ public class VCalendario extends javax.swing.JDialog {
         Njornada.setFont(new java.awt.Font("Dialog", 0, 18)); // NOI18N
         Njornada.setText("X");
 
-        pPartido1.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-
-        lbE1.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        lbE1.setText("Equipo1");
-
-        lbE2.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        lbE2.setText("Equipo2");
-
-        VS.setFont(new java.awt.Font("Dialog", 1, 14)); // NOI18N
-        VS.setText("VS");
-
-        lbFecha.setFont(new java.awt.Font("Dialog", 0, 14)); // NOI18N
-        lbFecha.setText("Fechaaaaaaa");
-
-        javax.swing.GroupLayout pPartido1Layout = new javax.swing.GroupLayout(pPartido1);
-        pPartido1.setLayout(pPartido1Layout);
-        pPartido1Layout.setHorizontalGroup(
-            pPartido1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pPartido1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(lbE1)
-                .addGap(32, 32, 32)
-                .addComponent(VS)
-                .addGap(32, 32, 32)
-                .addComponent(lbE2)
-                .addGap(70, 70, 70)
-                .addComponent(lbFecha)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
-        pPartido1Layout.setVerticalGroup(
-            pPartido1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(pPartido1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(pPartido1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(lbE1)
-                    .addComponent(lbE2)
-                    .addComponent(VS)
-                    .addComponent(lbFecha))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-        );
+        taPartidos.setColumns(20);
+        taPartidos.setRows(5);
+        jScrollPane1.setViewportView(taPartidos);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -100,9 +66,9 @@ public class VCalendario extends javax.swing.JDialog {
                         .addGap(18, 18, 18)
                         .addComponent(Njornada))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(101, 101, 101)
-                        .addComponent(pPartido1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(130, Short.MAX_VALUE))
+                        .addGap(98, 98, 98)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 393, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(100, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -111,9 +77,9 @@ public class VCalendario extends javax.swing.JDialog {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(Njornada))
-                .addGap(48, 48, 48)
-                .addComponent(pPartido1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(293, Short.MAX_VALUE))
+                .addGap(38, 38, 38)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 214, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(136, Short.MAX_VALUE))
         );
 
         pack();
@@ -126,17 +92,18 @@ public class VCalendario extends javax.swing.JDialog {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel Njornada;
-    private javax.swing.JLabel VS;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel lbE1;
-    private javax.swing.JLabel lbE2;
-    private javax.swing.JLabel lbFecha;
-    private javax.swing.JPanel pPartido1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextArea taPartidos;
     // End of variables declaration//GEN-END:variables
 
     private void obtenerPartidos() {
-        
-        Main.BuscarPartidosPorJornada(); 
+        try {
+            Main.consultarTodasLasJornadas();
+        } catch (Exception ex) {
+            Logger.getLogger(VCalendario.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        partidos=Main.BuscarPartidosPorJornada(j); 
     }
 
 }
