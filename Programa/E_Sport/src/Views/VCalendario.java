8e73f4a6a176e0293f95b09398309febbcc71522
Yaiza <yaiza.garay@ikasle.egibide.org>
2018-05-16 10:39:34 +0200

    vcalendario

--- Programa/E_Sport/src/Views/VCalendario.java
+++ Programa/E_Sport/src/Views/VCalendario.java
@@ -56,8 +56,8 @@ public class VCalendario extends javax.swing.JDialog {
         imagen = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
-        setMaximumSize(new java.awt.Dimension(579, 560));
-        setMinimumSize(new java.awt.Dimension(579, 560));
+        setMaximumSize(new java.awt.Dimension(581, 596));
+        setMinimumSize(new java.awt.Dimension(581, 596));
         getContentPane().setLayout(null);
 
         jLabel1.setFont(new java.awt.Font("Berlin Sans FB Demi", 0, 36)); // NOI18N
@@ -141,7 +141,7 @@ public class VCalendario extends javax.swing.JDialog {
 
         imagen.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/fondo2.jpg"))); // NOI18N
         getContentPane().add(imagen);
-        imagen.setBounds(0, 0, 580, 560);
+        imagen.setBounds(0, 0, 580, 600);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
