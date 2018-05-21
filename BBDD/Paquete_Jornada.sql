57a76439a33daa7b77cc682725e677ed6400a8e8
Mikel <mikelferreiroguridi@gmail.com>
2018-05-21 12:57:41 +0200

    paquete jornada

--- BBDD/Paquete_Jornada.sql
+++ BBDD/Paquete_Jornada.sql
@@ -6,6 +6,7 @@ CREATE OR REPLACE PACKAGE Pkg_Jornada IS
       TYPE TCURSOR IS REF CURSOR;
 -- Declaramos el procedimiento PartidosPorJornada con el parametro de entrada P_id_jor de tipo Integer y de salida C_partidos de tipo TCURSOR
       PROCEDURE PartidosPorJornada (P_id_jor integer,C_partidos OUT TCURSOR);
+      PROCEDURE Jornadas (C_jornadas OUT TCURSOR);
 END Pkg_Jornada;
 /
 --Declaramos el cuerpo del paquete
@@ -22,24 +23,41 @@ CREATE OR REPLACE PACKAGE BODY Pkg_Jornada IS
       AND M.Id_partido in (SELECT Id_partido FROM Partido WHERE Id_jornada = P_id_jor)
       ORDER BY M.Id_partido,M.Visitante;
   END;
+  PROCEDURE Jornadas (C_jornadas OUT TCURSOR) AS
+  BEGIN
+-- Abrimos el cursor
+  OPEN C_jornadas for
+--Llenamos el cursor con la id de jornada el Id de partido y equipo, su nombre su "lugar" y si es visitante o no
+      SELECT J.Id_jornada, J.Fecha_inicio, J.Fecha_fin, P.Id_partido, P.Fecha, E.Lugar , E.Id_equipo, E.Nombre, E.Comentario, M.Puntuacion, M.Visitante
+      FROM Equipo E, MARCADOR M, Partido P,Jornada J
+      WHERE E.Id_equipo = M.Id_equipo
+      AND P.Id_partido = M.Id_partido
+      AND P.Id_jornada = J.Id_jornada
+      ORDER BY J.Id_jornada,M.Id_partido,M.Visitante;
+  END;
 END Pkg_Jornada;
 /
 /*
 --Procedimiento anónimo de llamada
 DECLARE
       V_Cur  Pkg_Jornada.TCURSOR;
-      V_Visitante Marcador.Visitante%TYPE;
-      V_Id_partido Marcador.Id_partido%TYPE;
-      V_Id_equipo Equipo.Id_equipo%TYPE;
-      V_Nombre Equipo.Nombre%TYPE;     
+      V_id_jornada Jornada.Id_jornada%TYPE;
+      V_Fecha_inicio Jornada.Fecha_inicio%TYPE;
+      V_Fecha_fin Jornada.Fecha_fin%TYPE;
+      V_Id_partido partido.Id_partido%TYPE;
+      V_Fecha Partido.Fecha%TYPE;
       V_Lugar Equipo.Lugar%TYPE;
-      V_Id_jornada Partido.Id_jornada%TYPE:=1;
+      V_Id_equipo Equipo.Id_equipo%TYPE;
+      V_Nombre Equipo.Nombre%TYPE;
+      V_Comentario Equipo.Comentario%TYPE;
+      V_Puntuacion Marcador.Puntuacion%TYPE;
+      V_Visitante Marcador.Visitante%TYPE;     
 BEGIN
-    Pkg_Jornada.PartidosPorJornada( V_Id_jornada,V_Cur);
+    Pkg_Jornada.Jornadas(V_Cur);
     LOOP
-        FETCH V_Cur INTO V_Id_partido,V_Id_equipo,V_Nombre,V_Visitante, V_Lugar;
+        FETCH V_Cur INTO V_id_jornada,V_Fecha_inicio,V_Fecha_fin,V_Id_partido,V_Fecha,V_Lugar,V_Id_equipo,V_Nombre,V_Comentario,V_Puntuacion,V_Visitante ;
         EXIT WHEN V_CUR%NOTFOUND;
-        DBMS_OUTPUT.PUT_LINE(V_Id_partido|| ' ' || V_Id_equipo || ' ' || V_Nombre || ' ' || V_Visitante|| ' ' ||V_Lugar);
+        DBMS_OUTPUT.PUT_LINE(V_id_jornada|| ' ' ||V_Fecha_inicio|| ' ' ||V_Fecha_fin|| ' ' ||V_Id_partido|| ' ' ||V_Fecha|| ' ' ||V_Lugar|| ' ' ||V_Id_equipo|| ' ' ||V_Nombre|| ' ' ||V_Comentario|| ' ' ||V_Puntuacion|| ' ' ||V_Visitante);
     END LOOP;
 END;
 */
